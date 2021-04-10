fab7bb9ea2feab1d3db0e0c257314f7d4c50dd1d
Andrea <andreabonatowork@gmail.com>
2021-04-10 18:07:13 -0400

    added stuff to make css appear. Needs button funtionaity and login/signup stuff

--- chat/urls.py
+++ chat/urls.py
@@ -1,5 +1,8 @@
 # chat/urls.py
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from . import views
 
@@ -7,4 +10,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('mainpage/', views.mainpage, name='mainpage'),
     path('<str:room_name>/', views.room, name='room'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
